eece5d4ae6531cb28e30441622bc685f8f57769e
Roland D. San Nicolas <rdsannicolas@gmail.com>
2016-01-26 15:09:15 -0800

    video radio button now clears previous query

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,26 @@
 (function (module) {
   var videoList = {};
-
-  // videoList.all = []; do we need an array??
+  var videoHeight = 400;
+  var videoWidth = 500;
 
   $('#html').on('click', function() {
-    console.log('HTML clicked!');
+    // console.log('HTML clicked!');
+    $('#VideoResults').empty();
     videoList.requestVideos = function() {
         $.get(
           'https://www.googleapis.com/youtube/v3/search',{
           part: 'snippet',
           maxResults: 3,
-          q: 'html',
+          q: 'html+learn+programming',
           order: 'viewCount',
           key: 'AIzaSyCPIPcf2n-VyIyKO8KpMMMv5Ap2VxU_Tis'},
           function(data) {
             var videoOutput;
             $.each(data.items, function(i, item) {
-              console.log(item);
+              // console.log(item);
               videoTitle = item.snippet.title;
               videoId = item.id.videoId;
-              videoOutput = '<li><iframe src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+              videoOutput = '<li><iframe height="'+videoHeight+'" width="'+videoWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
               $('#VideoResults').append(videoOutput);
             });
           }
@@ -29,27 +30,159 @@
   });
 
   $('#css').on('click', function() {
-      console.log('CSS clicked!');
+      // console.log('CSS clicked!');
+      $('#VideoResults').empty();
+      videoList.requestVideos = function() {
+          $.get(
+            'https://www.googleapis.com/youtube/v3/search',{
+            part: 'snippet',
+            maxResults: 3,
+            q: 'css+learn+programming',
+            order: 'viewCount',
+            key: 'AIzaSyCPIPcf2n-VyIyKO8KpMMMv5Ap2VxU_Tis'},
+            function(data) {
+              var videoOutput;
+              $.each(data.items, function(i, item) {
+                // console.log(item);
+                videoTitle = item.snippet.title;
+                videoId = item.id.videoId;
+                videoOutput = '<li><iframe height="'+videoHeight+'" width="'+videoWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+                $('#VideoResults').append(videoOutput);
+              });
+            }
+          )
+        }
+      videoList.requestVideos();
   });
 
   $('#javascript').on('click', function() {
-      console.log('JavaScript clicked!');
+      // console.log('JavaScript clicked!');
+      $('#VideoResults').empty();
+      videoList.requestVideos = function() {
+          $.get(
+            'https://www.googleapis.com/youtube/v3/search',{
+            part: 'snippet',
+            maxResults: 3,
+            q: 'javascript+learn+programming',
+            order: 'viewCount',
+            key: 'AIzaSyCPIPcf2n-VyIyKO8KpMMMv5Ap2VxU_Tis'},
+            function(data) {
+              var videoOutput;
+              $.each(data.items, function(i, item) {
+                // console.log(item);
+                videoTitle = item.snippet.title;
+                videoId = item.id.videoId;
+                videoOutput = '<li><iframe height="'+videoHeight+'" width="'+videoWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+                $('#VideoResults').append(videoOutput);
+              });
+            }
+          )
+        }
+      videoList.requestVideos();
   });
 
   $('#ruby').on('click', function() {
-      console.log('Ruby clicked!');
+      // console.log('Ruby clicked!');
+      $('#VideoResults').empty();
+      videoList.requestVideos = function() {
+          $.get(
+            'https://www.googleapis.com/youtube/v3/search',{
+            part: 'snippet',
+            maxResults: 3,
+            q: 'ruby+learn+programming',
+            order: 'viewCount',
+            key: 'AIzaSyCPIPcf2n-VyIyKO8KpMMMv5Ap2VxU_Tis'},
+            function(data) {
+              var videoOutput;
+              $.each(data.items, function(i, item) {
+                // console.log(item);
+                videoTitle = item.snippet.title;
+                videoId = item.id.videoId;
+                videoOutput = '<li><iframe height="'+videoHeight+'" width="'+videoWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+                $('#VideoResults').append(videoOutput);
+              });
+            }
+          )
+        }
+      videoList.requestVideos();
   });
 
   $('#python').on('click', function() {
-      console.log('Python clicked!');
+      // console.log('Python clicked!');
+      $('#VideoResults').empty();
+      videoList.requestVideos = function() {
+          $.get(
+            'https://www.googleapis.com/youtube/v3/search',{
+            part: 'snippet',
+            maxResults: 3,
+            q: 'python+learn+programming',
+            order: 'viewCount',
+            key: 'AIzaSyCPIPcf2n-VyIyKO8KpMMMv5Ap2VxU_Tis'},
+            function(data) {
+              var videoOutput;
+              $.each(data.items, function(i, item) {
+                // console.log(item);
+                videoTitle = item.snippet.title;
+                videoId = item.id.videoId;
+                videoOutput = '<li><iframe height="'+videoHeight+'" width="'+videoWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+                $('#VideoResults').append(videoOutput);
+              });
+            }
+          )
+        }
+      videoList.requestVideos();
   });
 
   $('#objectivec').on('click', function() {
-      console.log('Objective C clicked!');
+      // console.log('Objective C clicked!');
+      $('#VideoResults').empty();
+      videoList.requestVideos = function() {
+          $.get(
+            'https://www.googleapis.com/youtube/v3/search',{
+            part: 'snippet',
+            maxResults: 3,
+            q: 'objective c+learn+programming',
+            order: 'viewCount',
+            key: 'AIzaSyCPIPcf2n-VyIyKO8KpMMMv5Ap2VxU_Tis'},
+            function(data) {
+              var videoOutput;
+              $.each(data.items, function(i, item) {
+                // console.log(item);
+                videoTitle = item.snippet.title;
+                videoId = item.id.videoId;
+                videoOutput = '<li><iframe height="'+videoHeight+'" width="'+videoWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+                $('#VideoResults').append(videoOutput);
+              });
+            }
+          )
+        }
+      videoList.requestVideos();
   });
 
   $('#swift').on('click', function() {
-      console.log('Swift clicked!');
+      // console.log('Swift clicked!');
+      $('#VideoResults').empty();
+      videoList.requestVideos = function() {
+          $.get(
+            'https://www.googleapis.com/youtube/v3/search',{
+            part: 'snippet',
+            maxResults: 3,
+            q: 'swift+learn+programming',
+            order: 'viewCount',
+            key: 'AIzaSyCPIPcf2n-VyIyKO8KpMMMv5Ap2VxU_Tis'},
+            function(data) {
+              var videoOutput;
+              $.each(data.items, function(i, item) {
+                // console.log(item);
+                videoTitle = item.snippet.title;
+                videoId = item.id.videoId;
+                videoOutput = '<li><iframe height="'+videoHeight+'" width="'+videoWidth+'" src=\"//www.youtube.com/embed/'+videoId+'\"></iframe></li>';
+                $('#VideoResults').append(videoOutput);
+              });
+            }
+          )
+        }
+      videoList.requestVideos();
   });
 
   module.videoList = videoList;
