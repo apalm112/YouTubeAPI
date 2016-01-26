a75614f7f33069b0f136ef69bacb12c66f7bce5d
apalm112 <apalm112@gmail.com>
2016-01-25 16:33:21 -0800

    Started coding for page2.js

--- vendor/scripts/pages.js
+++ vendor/scripts/pages.js
@@ -1,1 +1,16 @@
 
+// code to take input & pass to API request
+var userInput = function () {
+  $('inputOne').submit(function(e)( {
+    e.preventDefault();
+    $('').
+    );
+  }
+
+
+
+
+
+
+/*input class="inputOne"
+input class="inputTwo"*/
